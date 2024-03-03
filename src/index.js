2a0b2e48fba4d3ccbea97675a65f7e2483e4b6fc
Naza Sosa <142025821+nazasosaro@users.noreply.github.com>
2024-03-03 15:27:14 -0300

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-function refreshWearher(response) {
+function refreshWeather(response) {
+  let cityElement = document.querySelector("#weather-app-city-name");
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
   let temperaturElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#weather-app-city-name");
+  let conditionELement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   temperaturElement.innerHTML = Math.round(temperature);
+  conditionELement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+function formatDate(currentDate) {
+  console.log(currentDate);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+  let date = currentDate.getDate();
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return `${day} ${date}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
   // make api call and update the interface
   let apiKey = "5c47dto7c39e0337bb7ea2f5ff0be23a";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiURL).then(refreshWearher);
+  axios.get(apiURL).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
