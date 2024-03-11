6a11dd6c636abd4ca889617c99c613d63b5b6eb6
Naza Sosa <142025821+nazasosaro@users.noreply.github.com>
2024-03-10 23:31:38 -0300

    Inject the forecast HTML form JavaScript

--- src/index.js
+++ src/index.js
@@ -72,7 +72,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class="weather-forecast-day">
+      <div class="weather-forecast-date"><strong>${day}</strong></div>
+      <div class="weather-forecast-icon">🌧</div>
+      <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+          <strong>13°</strong>
+        </div>
+        <div class="weatherforecast">4°</div>
+      </div>
+    </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
+
+displayForecast();
